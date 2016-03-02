6cd655edd49a69a51fceff5dff6083527daa1edc
evanbemmelen <evavanbemmelen@gmail.com>
2016-03-02 10:01:23 +0100

    Kolommen & toggle

--- questions.js
+++ questions.js
@@ -1,5 +1,5 @@
 $('body').find('.vraag').click(function(){
-
+	
   //Expand or collapse this panel
   $(this).next().slideToggle('fast');
 
@@ -7,3 +7,24 @@ $('body').find('.vraag').click(function(){
   $(".antwoord").not($(this).next()).slideUp('fast');
 
 });
+
+$(document).ready(function(){
+    $(".vraag").hover(function(){
+        $("#footer").toggleClass(".main");
+    });
+});
+
+
+/*
+$("a").hover(function() {
+    // deze functie wordt gerund wanneer je over een link hovert.
+    // dan gaan we aan alle links een class toevoegen
+    $("a").addClass("main");
+    // behalve aan de link waar je over heen hovert
+    $(this).removeClass("main");
+}, function() {
+		// deze functie wordt gerund wanneer je je muis weer van de link weg-beweegt
+    // de class wordt overal weer weggehaald
+		$("a").removeClass("main");
+});
+*/
